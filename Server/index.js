fe3d742a6c6a03595d1f532d03405097538191be
EdsonCasta <dedsoncasta07@gmail.com>
2024-06-13 21:40:54 -0500

    conexion DB

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,9 @@
-//levantar el servidor
+const app = require('./src/app');
+const { conn } = require('./src/db')
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  conn.sync({ force: false });
+  console.log(`Listening on port ${PORT}`);
+});
--- Server/src/db.js
+++ Server/src/db.js
@@ -1,1 +1,19 @@
-//conexion base de datos
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const book = require("./models/book");
+const user = require("./models/user");
+
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ebookspalace`, {
+    logging: false,
+});
+
+const Book = book(sequelize);
+const User = user(sequelize);
+
+module.exports = {
+    conn: sequelize,
+    Book,
+    User
+};
