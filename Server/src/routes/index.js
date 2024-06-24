e49e919e45cdf67142c330a822e197f3bd6b54b0
GabTorres17 <g.abotorres@hotmail.com>
2024-06-24 12:03:28 -0400

    Stripe implementation

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -12,7 +12,8 @@ const { verifyUser } = require("../controllers/verifyUser");
 const cors = require("cors");
 const { putUserBan } = require('../controllers/putUserBanned');
 const { putUserCustomer } = require('../controllers/putUserCustomer');
-
+const stripe = require('stripe')('sk_test_51PUuD2P5B5kABXMb7qMmwaVcVSPvwoFGdllwCaaprxdcNKBeC4REXwKoQu2yGVYHDu6jKNONCG5GONOu989FnGt500n4RiJkmt');
+const YOUR_DOMAIN = 'http://localhost:5173';
 const router = Router();
 router.use(cors());
 
@@ -28,5 +29,25 @@ router.put('/carts/:id/status', putCartState);
 router.put('/users/:id/status/admin', putUserAdmin);
 router.put('/users/:id/status/ban', putUserBan);
 router.put('/users/:id/status/customer', putUserCustomer);
+
+router.post('/create-checkout-session', async (req, res) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            line_items: [
+                {
+                    price: 'price_1PUyaPP5B5kABXMbkCe9eLqW',
+                    quantity: 1,
+                },
+            ],
+            mode: 'payment',
+            success_url: `${YOUR_DOMAIN}?success=true`,
+            cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+        });
+
+        res.json({ url: session.url });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 module.exports = router;
